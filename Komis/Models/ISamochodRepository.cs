1f624059d0d15f0cacbd9a41d7a25261819fa286
ortofone24 <ortofone24@gmail.com>
2019-02-19 00:12:41 +0100

    Modele i Kontroller

---
+++ Komis/Models/ISamochodRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Komis.Models
+{
+    public interface ISamochodRepository
+    {
+        IEnumerable<Samochod> PobierzWszystkieSamochody();
+
+        Samochod PobierzSamochodoId(int samochodId);
+    }
+}
